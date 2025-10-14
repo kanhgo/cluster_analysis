d13dc60d2693d6411400b96634466ac9431c4f47
kanhgo <124202140+kanhgo@users.noreply.github.com>
2025-10-14 22:23:53 +0000

    Addressed use of strings in .py file

--- k_means_diagnostics.py
+++ k_means_diagnostics.py
@@ -1,13 +1,13 @@
-"""
-Reference the following for additional explanatory comments:
+###############################################
+# Reference the following for additional explanatory comments:
 
-.../Continuing Education - Lite/Machine Learning Ops/2-DevOps_DataOps_MLOps/
-MOD-2_Essential Maths and Data Science/3-Machine Learning and AI in Practice/15-Cluster_Analysis.ipynb
+# .../Continuing Education - Lite/Machine Learning Ops/2-DevOps_DataOps_MLOps/
+# MOD-2_Essential Maths and Data Science/3-Machine Learning and AI in Practice/15-Cluster_Analysis.ipynb
 
-.../Continuing Education - Lite/Machine Learning Ops/PRACTICE_ADD READINGS/
-WORKING WITH MULTIPLE PYTHON VERSIONS IN YOUR CODESPACE OR SERVER ENV
+# .../Continuing Education - Lite/Machine Learning Ops/PRACTICE_ADD READINGS/
+# WORKING WITH MULTIPLE PYTHON VERSIONS IN YOUR CODESPACE OR SERVER ENV
 
-"""
+################################################
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
@@ -21,13 +21,12 @@ df_num = df.drop(columns=["Trip_ID"])
 scaler = StandardScaler()
 df_scaled = scaler.fit_transform(df_num)
 
-"""
-EXPERIENCED COMPATABILITY ISSUES WITH yellowbrick IN THE DEFAULT ENVIRONMENT (PYTHON 3.12.x)
-I.E. ONE OF ITS DEPENDENCIES (DISTUTILS) WAS NOT COMPATABLE WITH PYTHON 3.12.x
-UTILIZED PYENV TO INSTALL AND MAKE ACCESSIBLE, ADDITIONAL PYTHON VERSIONS (3.10.x).
-CREATED A VIRTUAL ENVIRONMENT WITH PYTHON 3.10.x AND INSTALLED THE REQUIRED PACKAGES.
-
-"""
+##################################################
+# EXPERIENCED COMPATABILITY ISSUES WITH yellowbrick IN THE DEFAULT ENVIRONMENT (PYTHON 3.12.x)
+# I.E. ONE OF ITS DEPENDENCIES (DISTUTILS) WAS NOT COMPATABLE WITH PYTHON 3.12.x
+# UTILIZED PYENV TO INSTALL AND MAKE ACCESSIBLE, ADDITIONAL PYTHON VERSIONS (3.10.x).
+# CREATED A VIRTUAL ENVIRONMENT WITH PYTHON 3.10.x AND INSTALLED THE REQUIRED PACKAGES.
+##################################################
 
 # Use Elbow method to determine optimal number of clusters (k)
 from yellowbrick.cluster import KElbowVisualizer
@@ -60,21 +59,20 @@ plt.savefig("silhouette_analysis.png")
 # Close the plot object to ensure subsequent plots draw on completely clean canvas. Frees up memory.
 plt.close()
 
-"""
-REVIEW OF INITIAL DIAGNOSTICS
-The Elbow Method plot suggests that the optimal number of clusters (k) is 4 despite the synthetic data 
-being generated with 3 intentional clusters that match the "business logic". 
-This maybe indicative of an unintentional sub-cluster within one of the original clusters.
+####################################################
+# ##REVIEW OF INITIAL DIAGNOSTICS##
+# The Elbow Method plot suggests that the optimal number of clusters (k) is 4 despite the synthetic data 
+# being generated with 3 intentional clusters that match the "business logic". 
+# This maybe indicative of an unintentional sub-cluster within one of the original clusters.
 
-The Silhouette Analysis plot for k=3 shows that all samples have a positive silhouette score. 
-A score above 0.4 is generally considered decent for real-world data. 
+# The Silhouette Analysis plot for k=3 shows that all samples have a positive silhouette score. 
+# A score above 0.4 is generally considered decent for real-world data. 
 
-NEXT STEPS
-1. Experiment with k=4 in the Silhouette Analysis to see if it improves the average silhouette score.
-2. Use the KMeans model with the chosen k to fit the data and predict cluster assignments.
-3. Analyze the characteristics of each cluster to understand their differences and similarities.
-
-"""
+# NEXT STEPS
+# 1. Experiment with k=4 in the Silhouette Analysis to see if it improves the average silhouette score.
+# 2. Use the KMeans model with the chosen k to fit the data and predict cluster assignments.
+# 3. Analyze the characteristics of each cluster to understand their differences and similarities.
+#################################################
 
 # Create a KMeans instance with 4 clusters
 model = KMeans(n_clusters=4, random_state=42)
@@ -89,12 +87,12 @@ plt.savefig("silhouette_analysis_k4.png")
 # Close the plot object to ensure subsequent plots draw on completely clean canvas. Frees up memory.
 plt.close()
 
-"""
-The Silhouette Analysis for k=4 shows a siignificant decrease in the average score to approximately 0.34 from 
-0.42 (with k=3). This suggests that k=3 is a better choice for this dataset, aligning with the original
-data generation logic.
-An intercluster distance map could be useful to further evaluate the cluster separation.
-"""
+###############################################
+# The Silhouette Analysis for k=4 shows a siignificant decrease in the average score to approximately 0.34 from 
+# 0.42 (with k=3). This suggests that k=3 is a better choice for this dataset, aligning with the original
+# data generation logic.
+# An intercluster distance map could be useful to further evaluate the cluster separation.
+################################################
 
 from yellowbrick.cluster import InterclusterDistance
 
@@ -109,10 +107,12 @@ plt.savefig("ICD_analysis_k3.png")
 # Close the plot object to ensure subsequent plots draw on completely clean canvas. Frees up memory.
 plt.close()
 
-"""
-Intercluster Distance Map for k=3 shows that the clusters are distinct and well separated with no overlap.
-The proportions reflect the original data generation logic.
-"""
+#################################################
+# Intercluster Distance Map for k=3 shows that the clusters are distinct and well separated with no overlap.
+# The proportions reflect the original data generation logic.
+#################################################
+
+
 
 ## ELBOW METHOD FROM SCRATCH (WITHOUT YELLOWBRICK; PYTHON 3.12 COMPATIBLE))
 # import matplotlib.pyplot as plt
